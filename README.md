fc73de45b44e52eb5f6606669b4adfc5f885a0c7
caroquintana <caro.quintana.n@gmail.com>
2017-07-30 18:05:16 -0400

    creacion readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# pinterest
-Proyecto final Sprint-6
+## PINTEREST
+### Proyecto final sprint 6
+
+Replicar una vista estándar de Pinterest exhibiendo un tablero con 40 imágenes.
+
+**Requerimientos Técnicos**
+Recorrer un archivo JSON para sacar la información requerida:
+
+   - Título
+   - Descripción
+   - Nombre de usuario
+   - Hashtag
+   - Imagen
+
+Desarrollar vista principal en versión desktop (tamaño grande y extra-grande).
+Diagramción fluida, sin respetar columnas, simulando el comportamiento de despliegue de las imágenes de Pinterest.
+Cargar 20 imágenes inicialmente. Al hacer scroll, se cargarán las 20 siguientes.
+Al hacer click en la imagen, se debe desplegar un modal con el diseño entregado. La información se trae desde el JSON.
+
+**El proyecto fue realizado con las siguientes dependencias NPM:**
+
+    Dependencias de Desarrollo
+       - Gulp ~v.3.9.1
+       - Gulp-Concat ~v.2.6.1
+       - Gulp-Minify-CSS ~v.1.2.4
+       - Gulp-Sass ~v.3.0.0
+       - Gulp-Uglify ~v.2.0.0
+
+    Dependencias de Producción
+       - jQuery ~v.3.1.1
+       - Materialize ~v.0.97.8
+
+**Levantamiento ambiente de desarrollo**
+
+    Clonar repositorio.
+    Posicionado en nuestro proyecto hacer correr npm install para descargar e instalar todas las dependencias utilizadas.
+    Correr gulp para que se realizen las tareas especificadas.
+    
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,207 +2,36 @@ $(document).ready(function() {
 	var pin_datos = [{"id":1,"title":"Fuke zisukje bu dibas sik.","description":"Jusola mud afeafu ti mu.","user":"Ernest Barber","image_url":"1.jpg","username":"nerdale","hashtag":"Chic"},{"id":2,"title":"Pegup netotuv vohemucu tac kipifi.","description":"Samcaro am vazcat bujgumwa no ruden.","user":"Cecilia Sharp","image_url":"2.jpg","username":"carlita","hashtag":"Hairstyles"},{"id":3,"title":"Vek dufpodvo sojuzgid ehi le.","description":"Jazfegmi cemzalsan bi li oti agojoki hiim.","user":"Lucinda Ray","image_url":"3.jpg","username":"nadiatap","hashtag":"Wedding"},{"id":4,"title":"Wisiz bibusbuc giji lesi kebise.","description":"Wetniwop pegniwiv gahcu fok aja mazorut vi ziicnu.","user":"Jose Lyons","image_url":"4.jpg","username":"bosquedeinvierno","hashtag":"Wedding"},{"id":5,"title":"It rij dutu meb jitansef.","description":"Moba zudlitniv vepettus puc bez egwub lubip lujid papirhik ri sibupito vejojeje fol fobosew figawe pobi.","user":"Alice Sharp","image_url":"5.jpg","username":"vale","hashtag":"Flowers"},{"id":6,"title":"Tekcubmu banaroj tupjapti su holkug.","description":"Liszamzak bani jakvu bif suenru cakanum degemap zupek pedojgi kibogu paupuno rarnaj dok hiup jigu.","user":"Theresa Bush","image_url":"6.jpg","username":"nadiatap","hashtag":"Hippie"},{"id":7,"title":"Kivka ohfow atopede lobja tiraveara.","description":"Hufisu suf ekkalic focinzar elo jofoc gemmezdi cunzera.","user":"Caleb Malone","image_url":"7.jpg","username":"nerdale","hashtag":"Decorations"},{"id":8,"title":"Ni vaolu cu ujdu wasumulis.","description":"Tit wozfeafa ku sa likikbaw fu kaglac kirvafzi cagsevaw hope lawbiri jedovku seba.","user":"Connor Bowen","image_url":"8.jpg","username":"vale","hashtag":"Hippie"},{"id":9,"title":"Ujecegez islu tocureti salzup ow.","description":"Pa uce jaw holse mocka mogokobi egued zimmu.","user":"Harriett Frank","image_url":"9.jpg","username":"vreco","hashtag":"Rings"},{"id":10,"title":"Guto juwapan pe zi ejozico.","description":"Toaw vivfo jajgu cujnolsah hul ezogez lelo beji ute mo fujfasmez ol ko.","user":"Louisa Brown","image_url":"10.jpg","username":"gabrielista","hashtag":"Chic"},{"id":11,"title":"Vapmi nuhutu hu pav onidib.","description":"Lefi resawi osne evkocuv hez ehuep.","user":"Sara Jensen","image_url":"11.jpg","username":"bosquedeinvierno","hashtag":"Flowers"},{"id":12,"title":"Uja raf douga zanaco neib.","description":"Nekekub zapatas di fituf teot ge ujzahok soajnag ujvehso bu tumiz nobobsim toocava.","user":"Blanche Alvarez","image_url":"12.jpg","username":"marcia","hashtag":"Photography"},{"id":13,"title":"Kevubpul zoposiv samucli delpote zu.","description":"Vali giguvre pakuzav jusawda vikiw etnugev vawiro orocib bezzaw rigeg firab pek ukfoaze dewji ek.","user":"Lina Torres","image_url":"13.jpg","username":"nadiatap","hashtag":"Ideas"},{"id":14,"title":"Nifikgel zekunap hanag omjilwu pil.","description":"Wecca orgu rarip zoha uptomu herficvo zudpobu za kikunive lon givvotza iwvevi id.","user":"Phoebe Davidson","image_url":"14.jpg","username":"vreco","hashtag":"Dresses"},{"id":15,"title":"Lat olohi pirusoj nocilo ekagu.","description":"Fihdoccab ububiwme juc ve gedda nutu id pu.","user":"Blake Jimenez","image_url":"15.jpg","username":"nadiatap","hashtag":"Planning"},{"id":16,"title":"Ku zipaku lecom olegavu der.","description":"Jidefi kippi at canaben lad ror zefu otasuzo uhuriate metgazo vonnuwed piwov ossaj binmi wauc hoibebub rotu mecwuw.","user":"Alan Adkins","image_url":"16.jpg","username":"dulcefru","hashtag":"Wedding"},{"id":17,"title":"Weokuk leb onovogud lehimkah ukowanrec.","description":"Ceczo ifku kafwuc duh pelge kete jo fa pikbo cuk nur ti jukporpi etil ovsa hocepiw.","user":"Lucille Boyd","image_url":"17.jpg","username":"yugo_opresor","hashtag":"Ceremony"},{"id":18,"title":"Uwapic suw lopgo guvot sud.","description":"Zaza kewuwuz akepife wa rawin komlu jodzow iko sessoih tezkeduf lenlogi ufpab lumjafat zet ruev vor ek.","user":"Lewis Bell","image_url":"18.jpg","username":"gabrielista","hashtag":"Hairstyles"},{"id":19,"title":"Cuvuew bidrada gag gudipu oj.","description":"Galko of kabuzupe kougda pun hek cen vozerolo dafkadbih titekwov imu we zugoso zeblouz upa vut ra mujihici fer.","user":"Luella Banks","image_url":"19.jpg","username":"bosquedeinvierno","hashtag":"Dresses"},{"id":20,"title":"Ihtiz vamheket neaviva wiahu uloka.","description":"Vobu pevcurpum mimjectu demnu.","user":"Andre Jones","image_url":"20.jpg","username":"vreco","hashtag":"Dresses"},{"id":21,"title":"Zim pareih pizten evu tu.","description":"Daekdiv wewisut ze.","user":"Irene Curtis","image_url":"21.jpg","username":"vale","hashtag":"Invitations"},{"id":22,"title":"Vazi vi efde gubi evo.","description":"Gid nilvu.","user":"Bryan Little","image_url":"22.jpg","username":"nerdale","hashtag":"Ideas"},{"id":23,"title":"Uvajopwo cu ogawu dudnip heg.","description":"Birokli fapav zelsedjo lefocsu jicuge kozdof ne efi rav sevukupi ijhef ho ov mutomi gessad bowizoveg vojigtul.","user":"Tyler Horton","image_url":"23.jpg","username":"vale","hashtag":"Hippie"},{"id":24,"title":"Ges kodi cerruw nu emnuc.","description":"Idedet usubke buzti vega use va adufav lade.","user":"Brandon Roy","image_url":"24.jpg","username":"bosquedeinvierno","hashtag":"Cakes"},{"id":25,"title":"Ahu namam abo noashu bichofek.","description":"Iti bugpo sepu jugdifveb con nawkorgav fokzodac purujsa vufhal heusi riszaka fibpuja folozaejo.","user":"Nicholas Powers","image_url":"25.jpg","username":"gabrielista","hashtag":"Hairstyles"},{"id":26,"title":"Visoban depef emcajvul isodib jizu.","description":"Suosuko menasepi ufoogase cufveca ontifet fidahi tubopzom vifkooj dobuh na sezo tepguf cacocoku fenezu wozdiute.","user":"Helena Tyler","image_url":"26.jpg","username":"nadiatap","hashtag":"Photography"},{"id":27,"title":"Iha ni hocoj cicizhu umi.","description":"Desotmiz nivuhielu niv wu ze fe hancifi abidih wad ivehedeb doboguv oc rizumec.","user":"Genevieve Hall","image_url":"27.jpg","username":"dulcefru","hashtag":"Ideas"},{"id":28,"title":"Walzod vovlus le mar co.","description":"Vovfa.","user":"Rose Clayton","image_url":"28.jpg","username":"carlita","hashtag":"Hippie"},{"id":29,"title":"Ci kevvef bipolu ti oha.","description":"Cozdav im fupero oh uk sojnoh rubabe ruefediv.","user":"Calvin Fowler","image_url":"29.jpg","username":"onlyblank","hashtag":"Chic"},{"id":30,"title":"Uldujbu potre ocevi datapci cuftomho.","description":"Selpica ni kawof fi.","user":"Norman Daniel","image_url":"30.jpg","username":"yugo_opresor","hashtag":"Ceremony"},{"id":31,"title":"Win fitle duuh cijahoec oldugli.","description":"Zapo od jomgub kagjoz sa hid fesi ajlec.","user":"Alan Weber","image_url":"31.jpg","username":"bosquedeinvierno","hashtag":"Flowers"},{"id":32,"title":"Opod fic ravi hozieg bupu.","description":"Lek gomek follibmu vemazevaw fiske is vo jok tathavu punnus lahaose manum wicfev lovodo kani seiweici uzakijto abodi.","user":"Gertrude Day","image_url":"32.jpg","username":"marcia","hashtag":"Planning"},{"id":33,"title":"Nulaw iguzizlaf igiof me oh.","description":"Ditpij tare reicva kezovi eje nav ki peb vimupwe eguconaj napcuz zegsabzit peot ib safiz.","user":"Steven Osborne","image_url":"33.jpg","username":"yugo_opresor","hashtag":"Colors"},{"id":34,"title":"Duke wal ih unraksog de.","description":"Rezu fujifta suwuhtu nam ciwoidi ugeku viapeaze wab at ziz pe ra ospalwo vaco.","user":"Ruth Bowers","image_url":"34.jpg","username":"carlita","hashtag":"Wedding"},{"id":35,"title":"Zi ni siwe disepu liwevzev.","description":"Ocat hucoduka aldunte bumot heego jislias fijwupka ve.","user":"Todd Griffith","image_url":"35.jpg","username":"vreco","hashtag":"Hairstyles"},{"id":36,"title":"Ik dothi eszi se tulelu.","description":"Ditgi revsegaw suwa ihiwilfav hecera.","user":"Johnny Fisher","image_url":"36.jpg","username":"carlita","hashtag":"Dresses"},{"id":37,"title":"La wupojovi cod isi op.","description":"Ra.","user":"Elva Stephens","image_url":"37.jpg","username":"vale","hashtag":"Planning"},{"id":38,"title":"Efractij vo pomsu erulakrem mozjedfok.","description":"Jik miwu si huarute jaznuat noica iwriopi ehwosmeg mac zow te coodabom ib gunmous gambi eboco pavegip on miajuom.","user":"Irene Sparks","image_url":"38.jpg","username":"bosquedeinvierno","hashtag":"Photography"},{"id":39,"title":"Vuvil ib zap ul fefulo.","description":"Vaso nemi cu tej jiavpos zes ped abo ka lassivizi hibbite munegu adoisu cuuf.","user":"Clarence Stokes","image_url":"39.jpg","username":"onlyblank","hashtag":"Hairstyles"},{"id":40,"title":"Suzjak sunakvoc tuza zahovu sinsija.","description":"Ecruk ome su buma ruapdit wej onuawi wem pi.","user":"Minerva Welch","image_url":"40.jpg","username":"dulcefru","hashtag":"Colors"}];
 
 	pin_datos.forEach(function(element) {
-	    //console.log(element);
-	    //$('.photos').append('<img src="dist/img/' + element.image_url + '">');
-	    $('.totalfotos').append('<div class="singles"><img class="img_pinterest modal-trigger" href="#modal1" src="dist/img/' + element.image_url + '"><h3 class="title">'+ element.title +'</h3><p class="description">' + element.description + '</p><p class="user">'+'<i class="fa fa-user-circle" aria-hidden="true"></i> ' + element.user + '</p><p class="hashtag">' + '#'+ element.hashtag +'</p></div><div id="modal1" class="modal"><div class="modal-content center"></div><div class="modal-footer"></div>');
+	    $('.totalfotos').append('<div class="singles"><img class="img_pinterest modal-trigger" href="#modal1" src="dist/img/' + element.image_url + '"><h3 class="title">'+ element.title +'</h3><p class="description">' + element.description + '</p><p class="user">'+'<i class="fa fa-user-circle" aria-hidden="true"></i> ' + element.username + '</p><p class="hashtag">' + '#'+ element.hashtag +'</p></div><div id="modal1" class="modal"><div class="modal-content center"></div><div class="modal-footer"></div>');
 	});
-
 
 	$('.modal').modal();
 	$('#modal1').modal('close');
 
-	// for (var i = 0; i < pin_datos[i].length; i++) {
-	// 	var image = 
-	// }
-
-/*	pin_datos.forEach(function(fotitos) {
-	    $('.modal-content').append('<img class="img_modal" src="dist/img/' + fotitos.image_url + '">');
-	
-	});
-
-
-
-
-		$('.singles').on('click','img',function(){
-		var ruta = $(this).attr('src');
-		console.log(ruta);	
-	    $('.modal-content').append('<img class="img_modal" src=' + ruta + '>');
-
-		});
-
-
-
-			$('.singles').on('click','img',function(){
-		var ruta = $(this).attr('src');
-		console.log(ruta);
-
-		$.get(ruta, function(pin_datos){
-			console.log(pin_datos)
-			var contenedor ="";
-			var contenedor = contenedor + "<p><img src=" + ruta + "></p>";
-			
-			for(var i = 0; i < pin_datos.length; i++) { 
-                   var contenedor = contenedor + "<p>" + pin_datos.title + "</p>";
-                }
-		$('.modal-content').html(contenedor);
-
-		})	
-	    
-
-	});
-
-
-
-	$('.singles').on('click','img',function(){
-		var ruta = $(this).attr('src');
-		console.log(ruta);
-
-		$.get(ruta, function(pin_datos){
-			console.log(pin_datos)
-			var contenedor ="";
-			var contenedor = contenedor + "<p><img src=" + ruta + "></p>";
-			
-			for(var i = 0; i < pin_datos.length; i++) { 
-                   var contenedor = contenedor + "<p>" + pin_datos.title + "</p>";
-                }
-		$('.modal-content').html(contenedor);
-
-		})	
-	    
-
-	});
-
-
-	$('.singles').on('click','img',function(){
-		var ruta = $(this).attr('src');
-		console.log(ruta);	
-	    $('.modal-content').append('<img class="img_modal" src=' + ruta + '>');
-	    $('.modal-content').empty();
-
-		});
-
-
-
-
-var modalPinterest = function(pin_datos){
-	$(".modal-content").empty();
-	var template = "";
-	var url = "";
-	for(var i = 0; i < pin_datos.length; i++){
-			template += "<div class='row'>";
-		for (var j = 0; j < 4; j++){
-			url = pin_datos[i].image_url;
-			template += '<div class="col s3">';
-			template += '<a href="#modal-pokemon" data-enlace=' + url + ' class="pokemon center">';
-			template += '<article>'
-			template += '<img src="https://img.pokemondb.net/sprites/ruby-sapphire/normal/' + arrPokemon[4*i + j].name + '.png" class="responsive-img">';
-			template += '<p>' + arrPokemon[4*i + j].name;
-			template += '</p></article></a></div>'
-		}
-		template += "</div>"
-
-	}
-	$(".modal-content").append(template);
-	$(".singles").click(function(){
-		var url = $(this)[0].dataset.enlace;
-		llenarModal(url);
-	});
-}
-
-
-
-
-*/	
-
-
-
 	$('.singles').on('click',function(){
 		$('.modal-content').empty();
-		//var ruta = "";
 		var imagen = "";
 		var titulo = "";
 		var descripcion = "";
 		var usuario = "";
-		//var ruta = $(this).attr('src');
 		var imagen = $(this).find("img").attr("src");
 		var titulo = $(this).find("h3").text();
 		var descripcion = $(this).find(".description").text();
 		var usuario = $(this).find(".user").text();
 		console.log(imagen);
-		//$('.modal-content').append('<h4 class="titulo_modal">'+ titulo+'</h4><img class="img_modal" src=' + imagen + '>');
-		$('.modal-content').append('<div class="nav-modal"><ul class="nav-element"><li class="icon-modal"><i class="fa fa-share-square-o" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-check" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></li></ul><a href="#" class="btn-guardar"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Guardar</a></div><h4 class="titulo_modal">'+ titulo +'</h4><img class="img_modal" src=' + imagen + '><div class="nav-modal"><ul class="nav-element"><li class="icon-modal"><i class="fa fa-share-square-o" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-check" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></li></ul><a href="#" class="btn-guardar"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Guardar</a></div><p class="user-modal">'+ usuario +'</p>');	
-	    //$('.modal-content').append('<p>' + titulo + '</p>' );  
 
-		});
+		$('.modal-content').append('<div class="nav-modal"><ul class="nav-element"><li class="icon-modal"><i class="fa fa-share-square-o" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-check" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></li></ul><a href="#" class="btn-guardar"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Guardar</a></div><h4 class="titulo_modal">'+ titulo +'</h4><img class="img_modal" src=' + imagen + '><div class="nav-modal"><ul class="nav-element"><li class="icon-modal"><i class="fa fa-share-square-o" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-check" aria-hidden="true"></i></li><li class="icon-modal"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></li></ul><a href="#" class="btn-guardar"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Guardar</a></div><p class="user-modal"><i class="fa fa-user-circle" aria-hidden="true"></i> '+ usuario +'</p><a href="#" class="btn-leer">Leerlo</a><p class="descripcion-modal">' + descripcion + '</p>');
 
+	});
 
-
-
-
-
-        
-
-/*		$('.singles').on('click','img',function(){
-            var ruta = $(this).attr('src');
-          	console.log(ruta);
-          	$.get(ruta, function(pin_datos){
-          		var titulo = pin_datos.title;
-          		console.log(titulo)
-          	})
-          
-          }); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*	$('.totalfotos').onclick('click', 'img', function(){
-		$.get(pin_datos, function(traer){
-			var insertarInfo = "";
-			var	insertarInfo += "<h3 class='title'>"+ traer.title +"</h3>";
-			for (var i = 0; i < traer.types.length; i++) {
-			 	insertarInfo += "<div>" + traer.types[i].type.title + "</div>";
-			 }
-			$('.modal-content').html(insertarInfo); 
-		})
-	})*/
-
-
-
-
-
-
-	//
-/*
-	pin_datos.forEach(function(fotitos, index) {
-	    $('.modal-content').append('<img class="img_pinterest" src="dist/img/' + fotitos.image_url + '"><h3 class="title">'+ fotitos.title +'</h3><p class="description">' + fotitos.description + '</p><p class="user">'+'<i class="fa fa-user-circle" aria-hidden="true"></i> ' + fotitos.user + '</p>');
-	
-	}); */
-
-
-
-	/*	function(fotitos){
-        var insertarInfo = "";
-            insertarInfo += "<h3 class='title'>"+ fotitos.title +"</h3>"; 
-            insertarInfo += "<img class='img_pinterest' src='dist/img/" + fotitos.image_url + ">"; 
-            insertarInfo += "<p class='user'>"+"<i class='fa fa-user-circle' aria-hidden='true'></i> " + fotitos.user + "</p>";
-            insertarInfo += "<p class='description'>" + fotitos.description + "</p>"; 
-                
-
-                for(var i = 0; i < pin_datos.length; i++) { 
-                    insertarInfo += "<div>" +  + "</div>";
-                }
-                
-                $('.modal-content').html(insertarInfo);
-            };*/
-
+	$(window).scroll(function() {
+    	$.each($('img'), function() {
+        if ( $(this).attr('data-src') && $(this).offset().top < ($(window).scrollTop() + $(window).height() + 4) ) {
+            var source = $(this).data('src');
+            $(this).attr('src', source);
+            $(this).removeAttr('data-src');
+        	}
+    	})
+	})
 
 });
